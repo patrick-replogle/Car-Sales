54030f75911b2d437a941038ff4a64f70a791232
patrick-replogle <patrickr1138@gmail.com>
2019-11-16 09:42:32 -0800

    reducer logic in place. now to implement actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,5 @@
 export const BUY_ITEM = "BUY_ITEM";
-export const buyItem = item => {
-  return { type: BUY_ITEM, payload: item };
-};
+export const buyItem = item => ({ type: BUY_ITEM, payload: item });
 
 export const REMOVE_FEATURE = "REMOVE_FEATURE";
-export const removeFeature = item => {
-  return { type: REMOVE_FEATURE, payload: item };
-};
+export const removeFeature = item => ({ type: REMOVE_FEATURE, payload: item });
--- src/reducers/featureReducer.js
+++ src/reducers/featureReducer.js
@@ -19,6 +19,28 @@ const initialState = {
 
 export const featureReducer = (state = initialState, action) => {
   switch (action.type) {
+    case BUY_ITEM:
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice + action.payload.price,
+        car: {
+          ...state.car,
+          features: state.car.features.concat(action.payload)
+        },
+        additionalFeatures: state.additionalFeatures.filter(
+          item => item !== action.payload
+        )
+      };
+    case REMOVE_FEATURE:
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice - action.payload.price,
+        car: {
+          ...state.car,
+          features: state.car.features.filter(item => item !== action.payload)
+        },
+        additionalFeatures: [...state.additionalFeatures, action.payload]
+      };
     default:
       return state;
   }
